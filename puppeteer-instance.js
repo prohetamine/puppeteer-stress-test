94388efdf6d01bcb3833541df77d57e4d6499850
Stas Prohetamine <prohetamine@gmail.com>
2022-09-04 01:01:32 +0300

    save

--- puppeteer-instance.js
+++ puppeteer-instance.js
@@ -1,11 +1,44 @@
-const puppeteer = require('puppeteer')
+const { FakeBrowser } = require('fakebrowser')
+    , sleep = require('sleep-promise')
+    , path = require('path')
+    , userDataDir = path.resolve(__dirname, './user/fakeBrowserUserData'+Math.random())
 
-;(async () => {
-  setTimeout(() => {
-    process.exit()
-  }, 120000)
+!(async () => {
+    const windowsDD = require('./node_modules/fakebrowser/device-hub-demo/Windows.json')
 
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('https://example.com')
+    const builder = new FakeBrowser.Builder()
+        .deviceDescriptor(windowsDD)
+        .displayUserActionLayer(true)
+        .vanillaLaunchOptions({
+          headless: false,
+          executablePath: '/Users/stas/Work/Google\ Chrome.app/Contents/MacOS/Google\ Chrome',
+          userDataDir
+        })
+        .userDataDir(userDataDir);
+
+    const fakeBrowser = await builder.launch();
+
+    const oldPage = await fakeBrowser.getActivePage()
+    const page = await fakeBrowser.vanillaBrowser.newPage()
+    await oldPage.close()
+
+    global.userAction = fakeBrowser.userAction
+
+    await page.goto('https://walmart.com')
+
+    await page.goto('https://github.com')
+
+    await sleep(15000)
+
+    const button = await page.$('a[aria-label="Homepage"]');
+
+    await userAction.simClickElement(button)
+
+    await sleep(15000)
+
+    const link = await page.$('a[href="/signup?ref_cta=Sign+up&ref_loc=header+logged+out&ref_page=%2F&source=header-home"]');
+
+    await userAction.simClickElement(link)
+
+    await page.goto('https://prohetamine.ru')
 })()
